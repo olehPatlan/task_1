acd4893b0334935b0f18bcbd848872f6a230e59a
olehPatlan <olehpatlan@gmail.com>
2020-10-27 12:11:04 +0200

    Override Employee.equals() and Employee.hashCode()

--- src/main/java/ua/patlan/task1/Employee.java
+++ src/main/java/ua/patlan/task1/Employee.java
@@ -1,5 +1,7 @@
 package ua.patlan.task1;
 
+import java.util.Objects;
+
 public abstract class Employee {
     //unique identifier
     private final long id;
@@ -31,7 +33,7 @@ public abstract class Employee {
         return getWorkedHours() / STANDARD_WORKED_HOURS_PER_MONTH * hundredPercent;
     }
 
-    public double calculateSalary(){
+    public double calculateSalary() {
         int hundredPercent = 100;
         return getDefaultSalary() * percentageWorkedTime() / hundredPercent;
     }
@@ -66,5 +68,19 @@ public abstract class Employee {
 
     public void setWorkedHours(double workedHours) {
         this.workedHours = workedHours;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        //id is unique, so other fields can be skipped
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
