36e9c4df864866f165836cc45bb7feaba701628c
olehPatlan <olehpatlan@gmail.com>
2020-10-27 12:16:44 +0200

    Create test to Accountant.calculateEmployeesSalary()

--- src/test/java/TestTask1.java
+++ src/test/java/TestTask1.java
@@ -1,8 +1,14 @@
 import org.junit.Before;
 import org.junit.Test;
+import ua.patlan.task1.Accountant;
 import ua.patlan.task1.Employee;
 import ua.patlan.task1.Manager;
 import ua.patlan.task1.Programmer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -36,4 +42,20 @@ public class TestTask1 {
         assertNotEquals(manager, new Programmer(1));
         assertNotEquals(programmer, new Programmer(3));
     }
+
+    @Test
+    public void testCalculateEmployeesSalary() {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(programmer);
+        employees.add(manager);
+
+        Accountant accountant = new Accountant();
+        Map<Employee, Double> actual = accountant.calculateEmployeesSalary(employees);
+
+        Map<Employee, Double> expected = new HashMap<>();
+        expected.put(programmer, 200.);
+        expected.put(manager, 200.);
+        assertEquals(actual, expected);
+    }
+
 }
